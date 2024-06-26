671a45d8845dc10bf0865d716e58a82ce7d88c8e
Fabio Flauzino <fabio@mfrural.com.br>
2024-06-26 18:17:34 -0300

    iniciando aplicação

---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,11 @@
+import { Button } from "../../components/Button/index.jsx"
+import { Container} from "./styles.js"
+
+export function Details() {
+  
+  return (
+    <Container>
+      <Button title="Voltar"/>
+    </Container>
+  )
+}
