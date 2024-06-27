d31597817db1b02a26eac64770f5e30447fd47f2
unknown <fabioflauzino@hotmail.com>
2024-06-27 20:46:26 -0300

    Criando interface de SignUp

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,14 @@ import GlobalStyles from "./styles/global";
 
 import theme from "./styles/theme";
 import { ThemeProvider } from "styled-components";
+import { SignUp } from "./pages/SignUp";
 
-import { Details } from "./pages/Details";
-import { Home } from "./pages/Home";
-import { SignIn } from "./pages/SignIn";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 );
